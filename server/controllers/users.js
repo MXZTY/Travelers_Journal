cf30191fbc0d6ad42fa5baf15d8226c079ca4dbc
joshua2216 <rusty.josh@gmail.com>
2019-04-08 16:33:26 -0600

    apikey in browse

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -48,8 +48,8 @@ module.exports = {
         //Generate Token
         const token = signToken(newUser);
         
-        // respond with token
-        res.status(200).json({ token });
+        // respond with token, user id, and api key
+        res.status(200).json({ token, apikey:newUser.local.apikey });
 
     }, 
 
