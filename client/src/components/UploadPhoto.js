86ab7f469f0e15ad5553f7cdc82dcd6e38dd187d
Cryston <crystonrobin@gmail.com>
2019-04-09 00:03:17 -0600

    validation

--- client/src/components/UploadPhoto.js
+++ client/src/components/UploadPhoto.js
@@ -2,14 +2,44 @@ import React from 'react';
 import './EditPhotoForm.css';
 
 class UploadPhoto extends React.Component{
+    state = {
+        title: "",
+        desc: "",
+        city: "",
+        iso: "",
+        cityCode: "",
+        country: "",
+        continent: "",
+        latitude: "",
+        longitude: "",
+        file: ""
+        
+    }
 handleChange = e => {
 
     const regex = /^(\s)|-?(-?\d+(\.\d+)?)$/;
 
     let flag = true;
-   
+
+    if(e.currentTarget.name === "title"){
+         this.setState({ title: e.target.value });
+    }else if(e.currentTarget.name === "description"){
+        this.setState({ desc: e.target.value });
+    }else if(e.currentTarget.name === "iso"){
+        this.setState({ city: e.target.value });
+    }else if(e.currentTarget.name === "city"){
+        this.setState({ iso: e.target.value });
+    }else if(e.currentTarget.name === "cityCode"){
+        this.setState({ cityCode: e.target.value });
+    }else if(e.currentTarget.name === "country"){
+                this.setState({ country: e.target.value });
+    }else if(e.currentTarget.name === "photo"){
+     
+        this.setState({ file: e.target.value });
+    }
 
 
+  
     if (e.currentTarget.name === "latitude") {
       if (e.target.value === "" || e.target.value === "-") {
       } else {
@@ -48,7 +78,80 @@ handleChange = e => {
     if (!flag) {
      e.target.value = (e.target.value).slice(0,e.target.value.length -1);
     }
+    
+    if (e.currentTarget.name === "latitude"){
+         this.setState({ latitude: e.target.value });
+    }else if (e.currentTarget.name === "longitude"){
+         this.setState({ longitude: e.target.value });
+    }
   };
+
+//form validation
+
+validate = (e) =>{
+    let flag = true;
+     if(this.state.title === ""){
+         flag=false;
+    }else{
+         flag=true;
+    }
+    if(this.state.desc === ""){
+         flag=false;
+    }else{
+         flag=true;
+    }
+    
+    if(this.state.city === ""){
+         flag=false;
+    }else{
+         flag=true;
+    }
+    
+    if(this.state.iso === ""){
+         flag=false;
+    }else{
+         flag=true;
+    }
+    
+     if(this.state.cityCode === ""){
+         flag=false;
+    }else{
+         flag=true;
+    }
+    
+    if(this.state.country === ""){
+          flag=false;
+    }else{
+         flag=true;
+    }
+    if(this.state.latitude === ""){
+          flag=false;
+    }else{
+         flag=true;
+    }
+    if(this.state.longitude === ""){
+          flag=false;
+    }else{
+         flag=true;
+    }
+    
+   if(this.state.file === ""){
+          flag=false;
+    }else{
+         flag=true;
+    }
+    
+    console.log(flag);
+   
+    if(!flag){
+         e.preventDefault();
+    }else{
+        return true;
+    }
+
+   
+}
+
     render(){
         
             return(
@@ -59,27 +162,27 @@ handleChange = e => {
                 <legend> Upload New Photo</legend>
                             
                             <label>Title</label>
-                            <input type='text' name='title' />
+                            <input type='text'onChange={this.handleChange} name='title' />
                             <label>Description</label>
-                            <input type='text' name='description' />
+                            <input type='text'onChange={this.handleChange} name='description' />
                             <label>City</label>
-                            <input type='text' name='city' />
+                            <input type='text' onChange={this.handleChange} name='city' />
                             <label>ISO </label>
-                            <input type='text' name='iso' />
+                            <input type='text' onChange={this.handleChange} name='iso' />
                             <label>City Code </label>
-                            <input type='text' name='cityCode' />
+                            <input type='text' onChange={this.handleChange} name='cityCode' />
                             <label>Country</label>
-                            <input type='text' name='country' />
+                            <input type='text' onChange={this.handleChange} name='country' />
                             <label>Continent</label>
-                            <input type='text' name='continent' />
+                            <input type='text' onChange={this.handleChange} name='continent' />
                             <label>Latitude</label>
                             <input type='text' onChange={ e=> this.handleChange(e)} name='latitude' />
                             <label>Longitude</label>
-                            <input type='text' onChange={this.handleChange} name='longitude' />
+                            <input type='text' name='longitude' />
                                 </div>
                             <div className="col-8">       
                             <label> Upload a Photo </label>
-                            <input type='file' name='photo' accept="image/jpeg, .jpg, image/png, .png" />
+                            <input type='file' onChange={this.handleChange}  name='photo' accept="image/jpeg, .jpg, image/png, .png" />
                             <label>Make</label> 
                             <input type='text' name='make' />
                             <label>Model </label>
@@ -92,7 +195,7 @@ handleChange = e => {
                             <input type='text' name='focal' />
                             <label>Exif ISO</label>
                             <input type='number' name='exifISO' /> <br/>
-                            <button type="submit">Submit </button>
+                            <button type="submit" >Submit </button>
                         </div>
                         </form>
                         <br />
