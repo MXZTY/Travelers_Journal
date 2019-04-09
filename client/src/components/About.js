905f7b811e8cdd7f0405673323e11cced1d62267
joshua2216 <rusty.josh@gmail.com>
2019-04-09 00:02:56 -0600

    minor changes

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,17 +9,21 @@ class About extends React.Component {
         return (
             <div className="bannerAbout">
                 <div>
-
-                    <h1>A Simple Travel Photo Website</h1>
-                    <h3> Created With React</h3>
                     <br/>
-                    <h2> By JOSHUA, SAMER, SARF, CRYSTON, JEHANZEB, MAXWELL.</h2>
-                    <br/><br/>
-                    <h3>Asg2 Details</h3>
-                    
+                    <h4>Issues:</h4>
+                    <h4>- We do not have the user authentication working on both uploads/deletes</h4>
+                    <h4>- For image uploading you need to have all of the fields filled in</h4>
+                    <br></br>
                     <h4>API Examples:</h4>
                     <h4>- <a href="http://wettoast.rocks:3001/api/images/">All images, /api/images</a></h4>
                     <h4>- <a href="http://wettoast.rocks:3001/api/image/deefb8ee-1fd9-408e-a3d7-a9557e1326f9">A specific image based on ID, /api/image/:id</a></h4>
+                    <br></br>
+                    <h4>Roles:</h4>
+                    <h4>Due to our design practice of using different repositories for each functionality, it may be difficult to evaluate our groups.</h4>
+                    <h4>Please see the following breakdown for an idea.</h4>
+                    <h4>Max/Sarf- Implemented oauth authentication as well as local strategy on server with form validation using Joi, and react redux form for login and signup, API. </h4>
+                    <h4>Josh/Zeb- implemented the infrastructure for Google Cloud Platform, backend hosting server, and website domain.</h4>
+                    <h4>Sam/Cryston- Responsible for image uploading, and provided assistance for authentication and API.</h4>
                     <br></br>
                     <h4>References:</h4>
                     <h4>- GitHub documentation .gitignore files from <a href="https://help.github.com/articles/ignoring-files/"> here</a></h4>
@@ -32,7 +36,7 @@ class About extends React.Component {
                     <h4>- Mongoose and MongoDB access tutorials from <a href="https://www.youtube.com/playlist?list=PLVBXNyNyLNq0jyDcfjOc9psQYK_leG52r"> here</a></h4>
                     <h4>- Inserting items into MongoDB from <a href="https://www.tutorialkart.com/nodejs/mongoose/insert-document-to-mongodb/"> here</a></h4>
                     <h4>- Generating random id's for images from <a href="https://www.npmjs.com/package/uuid"> here</a></h4>
-                    <br/>
+                    <br></br>
                         <Link to='/browse'>
                             <button>Browse</button>
                         </Link>
